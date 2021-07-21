f2674f35e8afc5470083cf0f260f3854239351c4
Nikkoutan <elenansalnikova@yandex.ru>
2021-07-21 23:28:39 +0300

    дз №4

--- src/sweetGift.java
+++ src/sweetGift.java
@@ -1,15 +1,26 @@
 //задаём класс для сладостей
 class sweets {
-    String name;
-    double weight;
-    double price;
-    String description;
+    private String name;
+    private double weight;
+    private double price;
+    private String description;
     //задаём конструктор с параметрами
-    sweets(String n, double w, double p, String d) {
+    public sweets(String n, double w, double p, String d) {
         name = n;
         weight = w;
         price = p;
         description = d;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String showSpec() {
+        return (name +" (" + description + "): " + weight*1000 + " г - " + price + " руб/кг");
+    }
+    public double howMuch(){
+        return price*weight;
     }
 }
 public class sweetGift {
@@ -21,16 +32,21 @@ public class sweetGift {
         sweets Marshmallow = new sweets("Fluffies",0.15,600, "средние кусочки маршмэллоу");
 
         //общая стоимость и вес подарка
-        double giftPrice = Candy.price* Candy.weight + Jelly.price* Jelly.weight+ Marshmallow.price* Marshmallow.weight;
-        double giftWeight = Candy.weight+ Jelly.weight+ Marshmallow.weight;
+        double giftPrice = 0;
+        double giftWeight = 0;
+        for (sweets sw in ){
+            giftWeight += sw.getWeight();
+            giftPrice += sw.howMuch()
+        }
+
 
         //вывод информации о подарке в консоль
         System.out.println("Общий вес подарка: "+ giftWeight*1000 + " г.");
         System.out.println("Стоимость подарка: "+giftPrice+" р.");
         System.out.println("Состав: ");
-        System.out.println(Candy.name +" (" + Candy.description + "): " + Candy.weight*1000 + " г - " + Candy.price + " руб/кг");
-        System.out.println(Jelly.name +" (" + Jelly.description + "): " + Jelly.weight*1000 + " г - " + Jelly.price + " руб/кг");
-        System.out.println(Marshmallow.name +" (" + Marshmallow.description + "): " + Marshmallow.weight*1000 + " г - " + Marshmallow.price + " руб/кг");
+        System.out.println(Candy.showSpec());
+        System.out.println(Jelly.showSpec());
+        System.out.println(Marshmallow.showSpec());
     }
 
 }
