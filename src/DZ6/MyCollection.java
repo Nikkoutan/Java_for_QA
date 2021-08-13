c286f49967dbdb1f712b629c62a0486c0a387ee4
Nikkoutan <elenansalnikova@yandex.ru>
2021-08-13 21:05:07 +0300

    дз №6 исправленное

--- src/DZ6/MyCollection.java
+++ src/DZ6/MyCollection.java
@@ -7,37 +7,49 @@ import java.util.*;
 
 public class MyCollection {
     public static void main(String[] args) {
-            //задаём путь к файлу
-            String filename = "/Users/pc/Documents/words.txt";
-            Path path = Paths.get(filename);
-            Scanner sc = null;
+        //задаём путь к файлу
+        String filename = "/Users/pc/Documents/words.txt";
+        Path path = Paths.get(filename);
+        Scanner sc = null;
 
-            //читаем из файла
-            try {
-                sc = new Scanner(path);
-            } catch (IOException e) {//обрабатываем возможную ошибку
-                System.out.println("Файл не может быть прочитан");
-                e.printStackTrace();
-            }
-            //парсим разделённые пробелом слова в список
-            List<String> wordList = new LinkedList<>();
-            while (sc.hasNext()) {
-                wordList.add(sc.next());
-            }
+        //читаем из файла
+        try {
+            sc = new Scanner(path);
+        } catch (IOException e) {//обрабатываем возможную ошибку
+            System.out.println("Файл не может быть прочитан");
+            e.printStackTrace();
+        }
+        //парсим разделённые пробелом слова в список
+        List<String> wordList = new LinkedList<>();
+        while (sc.hasNext()) {
+            wordList.add(sc.next());
+        }
+
+        if (wordList.isEmpty()) System.out.println("Файл пуст!");
+        else {
 
             //сортируем по алфавиту
             Collections.sort(wordList);
+            for (String i: wordList) System.out.println(i);
+            System.out.println(0);
 
             //в отсортированном виде одинаковые слова идут друг за другом. Считаем количество повторений, сохраняем в виде ключ(слово) - значение(кол-во)
             int count = 1;
             Map<String, Integer> wordStat = new HashMap<>();
-            for (int i = 0; i < wordList.size() - 1; i++) {
-                if (wordList.get(i).equals(wordList.get(i + 1))) {
-                    count++;
-                } else {
-                    wordStat.put(wordList.get(i), count);
-                    count = 1;
-                }
+            for (int i = 0; i < wordList.size() -1; i++) {
+               if (wordList.get(i).equals(wordList.get(i + 1))) {
+                   count++;
+               }
+               else {
+                        wordStat.put(wordList.get(i), count);
+                        count = 1;
+               }
+            }
+            if (wordList.get(wordList.size() -1).equals(wordList.get(wordList.size() -2))){
+                wordStat.put(wordList.get(wordList.size() -1), count);
+            }
+            else {
+                wordStat.put(wordList.get(wordList.size() -1), 1);
             }
 
             //выводим в консоль
@@ -48,9 +60,10 @@ public class MyCollection {
             //находим максимальное значение, выводим все слова с этим значением
             int maxValue = Collections.max(wordStat.values());
             System.out.print("Чаще всего встречается:\n");
-            for (String i : wordStat.keySet()) {
-                if (wordStat.get(i) == maxValue) System.out.println(i + " - " + wordStat.get(i) + " шт.");
+            for (String a : wordStat.keySet()) {
+                if (wordStat.get(a) == maxValue) System.out.println(a + " - " + wordStat.get(a) + " шт.");
             }
 
+        }
     }
 }
